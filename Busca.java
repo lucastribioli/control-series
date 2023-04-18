ee875cb0e507a0c9e37cd52fb70b4985ab23f8e4
lucastribioli <luk.tribioli.nunes@gmail.com>
2023-04-18 17:59:31 -0300

    adjustments

--- Busca.java
+++ Busca.java
@@ -1,20 +1,18 @@
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
-
 public class Busca {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-            .uri(URI.create("https://omdbapi.com/?t=matrix&apikey=352fe67e"))
-            .build();
+                .uri(URI.create("https://omdbapi.com/?t=matrix&apikey=352fe67e"))
+                .build();
 
-        client.sendAsync(request, BodyHandlers.ofString())
-            .thenApply(HttpResponse::body)
-            .thenAccept(System.out::println)
-            .join();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
 
     }
 
--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 # Descrição
 
 Projeto feito em Java para estudo de chamada de API, utilizando a linguagem.
+
+## Como rodar?
+
+Instale Java na versão 11.
